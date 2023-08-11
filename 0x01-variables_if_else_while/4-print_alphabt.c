c49b67175815ccdb62b0ed1cd1324f9253f5684a
sky2626 <alhassansalu1@gmail.com>
2023-08-11 22:30:29 +0000

    error fix

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,11 +14,11 @@ int main(void)
 
 	while (i < 123)
 	{
-			if (i != 101 && i != 113)
-			{
-						putchar(i);
-			}
-			i++;
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+		i++;
 	}
 	putchar(10);
 
